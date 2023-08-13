88e77723886132e2c54749e1354de06394470a03
Motoki Saito <motoki@saito.sfs.uni-tuebingen.de>
2023-08-13 20:17:25 +0200

    Bug fixed: Unrecognized bad lines
    It seems that bad lines, which had a different number of columns than
    others, were simply ignored in the last version. This bug was due to
    pandas.read_csv and resolved by the update of pandas from 1.3 to 1.4.
    Because of this bug fix of pandas, pyclx.pyclx.read_celex turned to be
    able to detect bad lines and return an error. In order to accommodate
    variable column numbers, depending on the number of rows to read, the
    function pyclx.pyclx.read_celex was updated to the great extent. Now,
    the function returns a pandas dataframe with the maximum number of
    columns of the data being loaded. When some rows have fewer elements
    (columns), missing values will be registered as empty strings.
    
    On branch master
    Changes to be committed:
          modified:   pyclx/__init__.py
          modified:   pyclx/pyclx.py
          new file:   tests/resources/gml.cd
          new file:   tests/resources/gml.readme
          modified:   tests/test_pyclx.py

--- pyclx/pyclx.py
+++ pyclx/pyclx.py
@@ -1,22 +1,49 @@
 import pandas as pd
 
-def read_celex (which, clx_dir, usecols=None, squeeze=False, index_col=None, skiprows=None):
-    clx = '{}/{}.cd'.format(clx_dir,which)
-    clx_rd = '{}/{}.readme'.format(clx_dir,which)
-    with open(clx_rd, 'r') as f:
-        clx_rd = f.readlines()
-    clx_rd = [ i.strip() for i in clx_rd if i.strip()!='' ]
-    
-    aaa = pd.Series([ i[0].isdigit() for i in clx_rd ])
-    first_digit_pos = aaa.index[aaa][0]
-    non_digit_pos = aaa.index[~aaa]
-    last_pos = non_digit_pos[non_digit_pos>first_digit_pos][0]
-    clx_rd = clx_rd[first_digit_pos:last_pos]
-    clx_rd = [ i.split() for i in clx_rd ]
-    clms = [ i[1] for i in clx_rd ]
-    clx = pd.read_csv(clx, sep='\\', header=None, names=clms, na_filter=False, usecols=usecols, index_col=index_col, skiprows=skiprows)
+def read_celex (which, clx_dir, usecols=None, squeeze=False, nrows=None):
+    clx = '{}/{}.cd'.format(clx_dir, which)
+    if nrows is None:
+        with open(clx, 'r') as f:
+            clx = f.readlines()
+    else:
+        with open(clx, 'r') as f:
+            clx = [ next(f) for _ in range(nrows) ]
+    clx = pd.Series(clx).str.strip()
+    clx = clx.str.split('\\', regex=False, expand=True).fillna('')
+
+    readme_path = '{}/{}.readme'.format(clx_dir, which)
+    cnames = find_colnames(readme_path, clen=clx.shape[1])
+    clx.columns = cnames
+
+    if not (usecols is None):
+        colpos = pd.Series(clx.columns).str.replace('[0-9]$','',regex=True).isin(pd.Series(usecols))
+        clx = clx.loc[:,colpos.to_list()]
+    if all(pd.Series(clx.columns).apply(lambda x: x[-1])=='0'):
+        clx.columns = pd.Series(clx.columns).apply(lambda x: x[:-1])
     if squeeze:
         clx = clx.squeeze('columns')
     return clx
+
+def find_colnames (readme_path, clen=None):
+    with open(readme_path, 'r') as f:
+        rd = f.readlines()
+    rd = pd.Series(rd).str.strip()
+    rd = rd.loc[rd.str.contains('^[0-9]{1,2}\\. +[A-Za-z]+$', regex=True)].reset_index(drop=True)
+    rd = rd.str.replace(' +', ' ', regex=True)
+    rd = rd.str.split('. ', regex=False, expand=True).rename(columns={0:'ID',1:'Name'})
+    assert ((rd.ID.astype(int) < rd.ID.astype(int).shift(-1)).iloc[:-1]).all()
+
+    dup = rd.loc[rd.Name.duplicated(keep='first'),:].copy()
+    dup['ID'] = range(rd.ID.astype(int).max()+1, rd.ID.astype(int).max()+len(dup)+1)
+    rd = pd.concat([rd, dup], axis=0).reset_index(drop=True)
+
+    d1 = rd.Name.duplicated(keep='first').astype(int)
+    d2 = (rd.Name+rd.Name.duplicated(keep='first').astype(str)).duplicated(keep='first').astype(int)
+    dup_ind = (d1 + d2).astype(str)
+    rd['Name'] = rd.Name + dup_ind
+
+    cnames = rd.Name.iloc[:clen]
+    cnames.name = None
+    return cnames
 
 
--- tests/test_pyclx.py
+++ tests/test_pyclx.py
@@ -1,20 +1,35 @@
-import numpy as np
 from pathlib import Path
 import pyclx.pyclx as px
+import pandas as pd
 import pytest
 
 TEST_ROOT = Path(__file__).parent
 clx_dir = str(TEST_ROOT / 'resources')
 
 pars_read_celex = [
-    (None, False, None, (100,7), np.int64),
-    (None, False, 'Word', (100,6), object),
-    (['Word'], False, None, (100,1), np.int64),
-    (['Word'], True, None, (100,), np.int64)
+    (None,           False, None, (51728, 50),  3),
+    (None,           False,   10, (   10, 20), 13),
+    (None,            True, None, (51728, 50),  3),
+    (None,            True,   10, (   10, 20), 13),
+    (['Head','Def'], False, None, (51728,  4),  3),
+    (['Head','Def'], False,   10, (   10,  2),  2),
+    (['Head','Def'],  True, None, (51728,  4),  3),
+    (['Head','Def'],  True,   10, (   10,  2),  2),
+    (['Head'],       False, None, (51728,  1),  1),
+    (['Head'],       False,   10, (   10,  1),  1),
+    (['Head'],        True, None, (51728,   ),  0),
+    (['Head'],        True,   10, (   10,   ),  0)
 ]
-@pytest.mark.parametrize('usecols, squeeze, index_col, shape, indtype', pars_read_celex)
-def test_read_celex (usecols, squeeze, index_col, shape, indtype):
-    clx = px.read_celex('gpw', clx_dir, usecols, squeeze, index_col)
+
+@pytest.mark.parametrize('usecols, squeeze, nrows, shape, unqclm', pars_read_celex)
+def test_read_celex (usecols, squeeze, nrows, shape, unqclm):
+    clx = px.read_celex('gml', clx_dir, usecols, squeeze, nrows)
     assert clx.shape==shape
-    assert clx.index.dtype==indtype
+    if isinstance(clx, pd.core.series.Series):
+        _unqclm = 0
+    else:
+        _unqclm = len(set(pd.Series(clx.columns).apply(lambda x: x[-1])))
+    assert _unqclm==unqclm
+
+
 
